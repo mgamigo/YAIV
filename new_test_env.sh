32237bcddf885535771fb45990548a4a88c497ac
mgamigo <mgamigo@users.noreply.github.com>
2022-12-20 19:27:30 +0100

    updated version of new_test_env that works

--- new_test_env.sh
+++ new_test_env.sh
@@ -8,7 +8,8 @@ rm -r test_env
 virtualenv test_env
 source test_env/bin/activate
 pip install --upgrade pip
-pip install ipykernel==6.17.1
+pip install ipykernel
+#pip install ipykernel==6.17.1
 python -m ipykernel install --user --name=test_env
 jupyter kernelspec list
 cd $DIR
