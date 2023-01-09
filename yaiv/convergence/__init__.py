546c94291018765adf2b5df34913d6da0ed422d8
mgamigo <mgamigo@users.noreply.github.com>
2023-01-09 17:22:04 +0100

    wannier90 convergence working

--- yaiv/convergence/__init__.py
+++ yaiv/convergence/__init__.py
@@ -1,5 +1,6 @@
 import yaiv.convergence.cutoff
 import yaiv.convergence.smearing
 import yaiv.convergence.phonons
+import yaiv.convergence.wannier
 #import yaiv.experimental.convergence.smearing
 #import yaiv.experimental.convergence.phonons
--- yaiv/convergence/wannier.py
+++ yaiv/convergence/wannier.py
@@ -1,16 +1,15 @@
 #PYTHON module for cutoff convergence analysis
 
 import re
-import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
 # WANNIER90****************************************************************************************
 
-def __read_w90out(file):
+def read_w90out(file):
     """reads w90out file in order to analyze the minimization
-    outputs:
-    return total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta
+    returns:
+    total_spread, final_spread_delta, wan_spreads, wan_centers, disentang, final_dis_delta
     """
     lines=open(file,'r')
     read_dis=False
@@ -93,196 +92,209 @@ def __read_w90out(file):
 
     return total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta
 
-def w90_wannierization(file,save_as=None):
+def w90_wannierization(data,grid=True,save_as=None,axis=None):
     """ Plots the evolution of the wannierization of the w90 routine (total spread).
-    Uses the .wout file as input
+    Uses either the .wout file or the output of read_w90out(file) as input.
     """
-    total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta=__read_w90out(file)    
-    plt.figure()
+    if type(data)==str:
+        data=read_w90out(data)
+
+    if axis == None:
+        fig=plt.figure()
+        ax = fig.add_subplot(111)
+        textsize=12
+    else:
+        ax=axis
+        textsize=10
+
+    total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta=data
+
     data=total_spread
     delta=np.hstack([0,np.diff(data[:,1])])
     data=np.insert(data,2,delta,axis=1)
-    plt.plot(data[:,0],data[:,1])
+    ax.plot(data[:,0],data[:,1])
     #plt.errorbar(data[:,0],data[:,1],yerr=data[:,2],elinewidth=0.4)
-
-    plt.text(data[-1,0]*0.8,data[0,1]-(data[0,1]-data[-1,1])*0.07
+    
+    ax.text(data[-1,0]*0.8,data[0,1]-(data[0,1]-data[-1,1])*0.07
              , 'Final $\Delta\Omega$ ='+str(spread_delta)
-             , size=12, ha="center", va="center",
+             , size=textsize, ha="center", va="center",
              bbox=dict(boxstyle="round",
                        ec=(1., 0.5, 0.5),
                        fc=(1., 0.8, 0.8),
                        )
              )
-    plt.grid(axis='both',color='0.95')   
-    plt.title('Wannerization')
-    plt.ylabel('$\Omega \ (\AA^2)$')
-    plt.xlabel('Iteration')
+    ax.set_title('Wannerization')
+    ax.set_ylabel('$\Omega \ (\AA^2)$')
+    ax.set_xlabel('Iteration')
 
-    plt.tight_layout()
+    if grid == True:
+        ax.grid(axis='both',color='0.95')   
     if save_as!=None:
-        plt.savefig(save_as,dpi=500)
-    plt.show()
+        plt.tight_layout()
+        plt.savefig(save_as,dpi=300)
+        plt.show()
+    if axis == None:
+        plt.tight_layout()
+        plt.show()
 
-def w90_disentanglement(file,save_as=None):
+def w90_disentanglement(data,grid=True,save_as=None,axis=None):
     """ Plots the disentanglement procedure of the w90 routine (the gauge invariant spread).
-    Uses the .wout file as input
+    Uses either the .wout file or the output of read_w90out(file) as input.
     """
-    total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta=__read_w90out(file)    
-    plt.figure()
-    data=disentang
-    plt.plot(data[:,0],data[:,1])
+    if type(data)==str:
+        data=read_w90out(data)
 
-    plt.text(data[-1,0]*0.8,data[0,1]-(data[0,1]-data[-1,1])*0.07
+    if axis == None:
+        fig=plt.figure()
+        ax = fig.add_subplot(111)
+        textsize=12
+    else:
+        ax=axis
+        textsize=10
+
+    total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta=data
+
+    data=disentang
+    ax.plot(data[:,0],data[:,1])
+
+    ax.text(data[-1,0]*0.8,data[0,1]-(data[0,1]-data[-1,1])*0.07
              , 'Final $\Delta\Omega_I$ ='+str(dis_delta)
-             , size=12, ha="center", va="center",
+             , size=textsize, ha="center", va="center",
              bbox=dict(boxstyle="round",
                        ec=(1., 0.5, 0.5),
                        fc=(1., 0.8, 0.8),
                        )
              )
     
-    plt.grid(axis='both',color='0.95')   
-    plt.title('Disentanglement')
-    plt.ylabel('$\Omega_I \ (\AA^2)$')
-    plt.xlabel('Iteration')
+    ax.set_title('Disentanglement')
+    ax.set_ylabel('$\Omega_I \ (\AA^2)$')
+    ax.set_xlabel('Iteration')
 
-    plt.tight_layout()
+    if grid == True:
+        ax.grid(axis='both',color='0.95')   
     if save_as!=None:
-        plt.savefig(save_as,dpi=500)
-    plt.show()
-    
-    
-def w90_centers(file,save_as=None):
+        plt.tight_layout()
+        plt.savefig(save_as,dpi=300)
+        plt.show()
+    if axis == None:
+        plt.tight_layout()
+        plt.show()
+
+def w90_centers(data,grid=True,save_as=None,axis=None):
     """ Plots the evolution of the wannier centers of the w90 routine.
-    Uses the .wout file as input
+    Uses either the .wout file or the output of read_w90out(file) as input.
     """
-    total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta=__read_w90out(file)    
-    plt.figure()
+    if type(data)==str:
+        data=read_w90out(data)
+
+    if axis == None:
+        fig=plt.figure()
+        ax = fig.add_subplot(111)
+    else:
+        ax=axis
+
+    total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta=data
+    
     data=wan_centers
     center_num=len(wan_centers[0,:])-1
     wan_num=len(wan_spreads[0,:])-1
 
-    plt.plot(data[:,0],data[:,1],color='C0',linewidth=0.7,label='x')
-    plt.plot(data[:,0],data[:,2],color='C1',linewidth=0.7,label='y')
-    plt.plot(data[:,0],data[:,3],color='C2',linewidth=0.7,label='z')
+    ax.plot(data[:,0],data[:,1],color='C0',linewidth=0.7,label='x')
+    ax.plot(data[:,0],data[:,2],color='C1',linewidth=0.7,label='y')
+    ax.plot(data[:,0],data[:,3],color='C2',linewidth=0.7,label='z')
 
 
     for n in range(4,center_num+1,3):
-        plt.plot(data[:,0],data[:,n],color='C0',linewidth=0.7)
+        ax.plot(data[:,0],data[:,n],color='C0',linewidth=0.7)
     for n in range(5,center_num+1,3):
-        plt.plot(data[:,0],data[:,n],color='C1',linewidth=0.7)
+        ax.plot(data[:,0],data[:,n],color='C1',linewidth=0.7)
     for n in range(6,center_num+1,3):
-        plt.plot(data[:,0],data[:,n],color='C2',linewidth=0.7)        
+        ax.plot(data[:,0],data[:,n],color='C2',linewidth=0.7)        
 
-    plt.grid(axis='both',color='0.95')   
-    plt.title('WFs centers ('+str(wan_num)+')')
-    plt.ylabel('Center coord. ($\AA$)')
-    plt.xlabel('Iteration')
-    plt.legend()
+    if axis==None:
+        ax.set_title('WFs centers ('+str(wan_num)+')')
+    else:
+        ax.set_title('WFs centers')
+    ax.set_ylabel('Center coord. ($\AA$)')
+    ax.set_xlabel('Iteration')
+    ax.legend()
 
-    plt.tight_layout()
+    if grid == True:
+        ax.grid(axis='both',color='0.95')   
     if save_as!=None:
-        plt.savefig(save_as,dpi=500)
-    plt.show()
-    
-def w90_spreads(file,save_as=None):
+        plt.tight_layout()
+        plt.savefig(save_as,dpi=300)
+        plt.show()
+    if axis == None:
+        plt.tight_layout()
+        plt.show()
+
+def w90_spreads(data,grid=True,save_as=None,axis=None):
     """ Plots the evolution of the individual spreads of the w90 routine.
-    Uses the .wout file as input
+    Uses either the .wout file or the output of read_w90out(file) as input.
     """
-    total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta=__read_w90out(file)    
-    plt.figure()
+    if type(data)==str:
+        data=read_w90out(data)
+
+    if axis == None:
+        fig=plt.figure()
+        ax = fig.add_subplot(111)
+    else:
+        ax=axis
+
+    total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta=data
+
     data=wan_spreads
     wan_num=len(wan_spreads[0,:])-1
     for n in range(1,wan_num+1):
-        plt.plot(data[:,0],data[:,n],color='C0',linewidth=0.7)
+        ax.plot(data[:,0],data[:,n],color='C0',linewidth=0.7)
     
-    plt.grid(axis='both',color='0.95')   
-    plt.title('WFs spreads ('+str(wan_num)+')')
-    plt.ylabel('$\Omega \ (\AA^2)$')
-    plt.xlabel('Iteration')
+    if axis==None:
+        ax.set_title('WFs spreads ('+str(wan_num)+')')
+    else:
+        ax.set_title('WFs spreads')
+    ax.set_ylabel('$\Omega \ (\AA^2)$')
+    ax.set_xlabel('Iteration')
 
-    plt.tight_layout()
+    if grid == True:
+        ax.grid(axis='both',color='0.95')   
     if save_as!=None:
-        plt.savefig(save_as,dpi=500)
-    plt.show()
+        plt.tight_layout()
+        plt.savefig(save_as,dpi=300)
+        plt.show()
+    if axis == None:
+        plt.tight_layout()
+        plt.show()
 
-def w90(file,save_as=None):
+
+def w90(data,grid=True,save_as=None):
     """ Plots the evolution of Wannierization routine.
-    Uses the .wout file as input
+    Uses either the .wout file or the output of read_w90out(file) as input.
     ax1: Wannierization of the w90 routine (total spread)
     ax2: Disentanglement procedure of the w90 routine (the gauge invariant spread)
     ax3: Evolution of the individual spreads of the w90 routine
     ax4: Evolution of the wannier centers of the w90 routine
     """
-    fig = plt.figure(figsize=(9.0, 7.0)) #size of our plots (in inches)
-    axes1=fig.add_subplot(2,2,1) #rows of sublots, colums of subplots, place of this figure
-    axes2=fig.add_subplot(2,2,2)
-    axes3=fig.add_subplot(2,2,3)
-    axes4=fig.add_subplot(2,2,4)
-    
-    total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta=__read_w90out(file)
+    if type(data)==str:
+        data=read_w90out(data)
 
-    data=total_spread
-    axes1.plot(data[:,0],data[:,1])
-    axes1.text(data[-1,0]*0.75,data[0,1]-(data[0,1]-data[-1,1])*0.07
-             , 'Final $\Delta\Omega$ ='+str(spread_delta)
-             , size=10, ha="center", va="center",
-             bbox=dict(boxstyle="round",
-                       ec=(1., 0.5, 0.5),
-                       fc=(1., 0.8, 0.8),
-                       )
-             )
-    axes1.grid(axis='both',color='0.95')   
-    axes1.set_title('Total Spread')
-    axes1.set_ylabel('$\Omega \ (\AA^2)$')
-    axes1.set_xlabel('Iteration')
-    
-    if dis_delta!=0:
-        data=disentang
-        axes2.plot(data[:,0],data[:,1])
-        axes2.text(data[-1,0]*0.75,data[0,1]-(data[0,1]-data[-1,1])*0.07
-                 , 'Final $\Delta\Omega_I$ ='+str(dis_delta)
-                 , size=10, ha="center", va="center",
-                 bbox=dict(boxstyle="round",
-                           ec=(1., 0.5, 0.5),
-                           fc=(1., 0.8, 0.8),
-                           )
-                 )
-        axes2.grid(axis='both',color='0.95')   
-        axes2.set_title('Disentanglement')
-        axes2.set_ylabel('$\Omega_I \ (\AA^2)$')
-        axes2.set_xlabel('Iteration')
-    else:
-        axes2.set_title('No Disentanglement')
-    
-    data=wan_spreads
+    total_spread,spread_delta,wan_spreads,wan_centers,disentang,dis_delta=data
     wan_num=len(wan_spreads[0,:])-1
-    for n in range(1,wan_num+1):
-        axes3.plot(data[:,0],data[:,n],color='C0',linewidth=0.7)
-    axes3.grid(axis='both',color='0.95')   
-    axes3.set_title('WFs spreads')
-    axes3.set_ylabel('$\Omega \ (\AA^2)$')
-    axes3.set_xlabel('Iteration')
-    
-    
-    data=wan_centers
-    center_num=len(wan_centers[0,:])-1
-    axes4.plot(data[:,0],data[:,1],color='C0',linewidth=0.7,label='x')
-    axes4.plot(data[:,0],data[:,2],color='C1',linewidth=0.7,label='y')
-    axes4.plot(data[:,0],data[:,3],color='C2',linewidth=0.7,label='z')
-    for n in range(4,center_num+1,3):
-        axes4.plot(data[:,0],data[:,n],color='C0',linewidth=0.7)
-    for n in range(5,center_num+1,3):
-        axes4.plot(data[:,0],data[:,n],color='C1',linewidth=0.7)
-    for n in range(6,center_num+1,3):
-        axes4.plot(data[:,0],data[:,n],color='C2',linewidth=0.7)        
-    axes4.grid(axis='both',color='0.95')   
-    axes4.set_title('WFs centers')
-    axes4.set_ylabel('Center coord. ($\AA$)')
-    axes4.set_xlabel('Iteration')
-    axes4.legend()
-    
+
+    fig = plt.figure(figsize=(9.0, 7.0)) #size of our plots (in inches)
+    ax1=fig.add_subplot(2,2,1) #rows of sublots, colums of subplots, place of this figure
+    ax2=fig.add_subplot(2,2,2)
+    ax3=fig.add_subplot(2,2,3)
+    ax4=fig.add_subplot(2,2,4)
+
+    w90_wannierization(data,grid=grid,axis=ax1)
+    w90_disentanglement(data,grid=grid,axis=ax2)
+    w90_spreads(data,grid=grid,axis=ax3)
+    w90_centers(data,grid=grid,axis=ax4)
+
     fig.suptitle('Wannierization ('+str(wan_num)+' WF)', fontsize=16)
     plt.tight_layout()
+    if save_as!=None:
+        plt.savefig(save_as,dpi=300)
     plt.show()
 
