69a6b09fc2b53f46eb58cf6398ff45f7c82d8587
mgamigo <mgamigo@users.noreply.github.com>
2023-05-10 18:01:59 +0200

    change for the version of python I use

--- new_test_env.sh
+++ new_test_env.sh
@@ -4,8 +4,8 @@ DIR=`pwd`
 cd /home/martin/Software/enviroments
 jupyter kernelspec uninstall test_env
 rm -r test_env
-#virtualenv --python=/usr/bin/python3.8 test_env
-virtualenv test_env
+virtualenv --python=/usr/bin/python3.10 test_env
+#virtualenv test_env
 source test_env/bin/activate
 pip install --upgrade pip
 #pip install ipykernel==6.17.1
