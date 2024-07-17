1cb3f18ef37ec7779d56ebe2600a2b4924d30d09
mgamigo <mgamigo@users.noreply.github.com>
2024-07-17 16:16:09 +0200

    add new function from QE displacements to polarization vectors

--- yaiv/experimental/phonon.py
+++ yaiv/experimental/phonon.py
@@ -991,6 +991,23 @@ def __qe_norm_factor2(QE_disp,masses):
         N=N+masses[i]*(np.linalg.norm(QE_disp[i]))**2
     return N
 
+def qe_disp2pol(QE_disp,masses):
+    """From QE normalized displacement vectors to polarization vectors.
+    You can feed either one or a list of polarization vectors.
+    return polarization_vectors
+    """
+    rank=len(np.shape(QE_disp))
+    pol=np.copy(QE_disp)
+    if rank==2:
+        pol=[pol]
+    for i,p in enumerate(pol):
+        for j,vec in enumerate(p):
+            pol[i][j]=vec*np.sqrt(masses[j])
+        pol[i]=pol[i]/(np.linalg.norm(pol[i]))
+    if rank==2:
+        return pol[0]
+    return pol
+
 def poli(x,coef):
     """Generates the y value at the x point for a polinomy defined by certain coeficients
     x = point to evaluate
